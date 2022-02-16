0c343562465f73e138ec4110e2f20f04643ba26e
politeperson <saul.rojas@ucsp.edu.pe>
2022-02-16 11:00:01 -0500

    initial components added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Libraries
+import React from "react";
+
+// Components
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default App;
